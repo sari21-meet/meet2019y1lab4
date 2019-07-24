2bac4c7419298d29601a31f908763391d7bb9848
sari21-meet <sari21@meet.mit.edu>
2019-07-24 19:55:07 +0300

    Stopping here

--- fruit_sorter.py
+++ fruit_sorter.py
@@ -1,1 +1,11 @@
+
 fruit = input('Choose a fruit!')
+if fruit == 'apple' :
+    print('Bin!')
+elif fruit == 'orange' :
+    print('Bin2!')
+elif fruit == 'banana' :
+    print('Bin3!')
+else :
+    print('Error! I do not recognize this fruit!')
+    
